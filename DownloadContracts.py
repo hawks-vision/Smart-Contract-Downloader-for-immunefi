421ebaab53961ff9ac74d2588c688595a22f9440
Kis3_H@wksvision <50712130+hawks-vision@users.noreply.github.com>
2023-10-11 13:57:17 +0530

    Update DownloadContracts.py

--- DownloadContracts.py
+++ DownloadContracts.py
@@ -4,11 +4,13 @@ import string
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 import argparse
+
 linkArr=[]
 parser= argparse.ArgumentParser()
 parser.add_argument("--explorer","-e",type=str,help="specify the explorer ex: etherscan",required=True)
 parser.add_argument("--projectname","-p",type=str,help="specify the name of the project as mentioned in immunefi ex: thegraph",required=True)
 args=parser.parse_args()
+
 def extractingContractLinks():
     req=Request(url = "https://immunefi.com/bounty/"+args.projectname+"/",headers={'User-Agent': 'Mozilla/5.0'})
     page = urlopen(req)
