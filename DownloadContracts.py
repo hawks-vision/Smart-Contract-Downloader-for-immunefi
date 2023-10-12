fc66c8546db175e6c97c25538f8f2188dc857e15
Kis3_H@wksvision <50712130+hawks-vision@users.noreply.github.com>
2023-10-12 14:13:10 +0530

    Add files via upload
    added an additional option for output directory

--- DownloadContracts.py
+++ DownloadContracts.py
@@ -9,6 +9,7 @@ linkArr=[]
 parser= argparse.ArgumentParser()
 parser.add_argument("--explorer","-e",type=str,help="specify the explorer ex: etherscan",required=True)
 parser.add_argument("--projectname","-p",type=str,help="specify the name of the project as mentioned in immunefi ex: thegraph",required=True)
+parser.add_argument("--out","-o",type=str,nargs="?", default="out",const="out",help="to specify the output directory(optional)")
 args=parser.parse_args()
 
 def extractingContractLinks():
@@ -16,14 +17,16 @@ def extractingContractLinks():
     page = urlopen(req)
     html = page.read().decode("utf-8")
     soup = BeautifulSoup(html, "html.parser")
-    for i in soup.find_all("a",{'href': re.compile(r'etherscan\.io/')}):
-        # print(i.get('title'))
+    for i in soup.find_all("a",{'href': re.compile(r'({0})'.format(args.explorer))}):
+        #print(i.get('title'))
         linkArr.append(i.get('title'))
     
 def DownloadingSmartContracts():
     for i in linkArr:
-        print(i.split('/')[-1])
-        os.system('ethereum-sources-downloader ' + args.explorer +' '+ i.split('/')[-1])
+        head,sed,tail=(i.split('/')[-1]).partition('?')
+        # print(head)
+        # print(args.out)
+        os.system('ethereum-sources-downloader ' + args.explorer +' '+ head+ ' ' +args.out)
 
 
 
